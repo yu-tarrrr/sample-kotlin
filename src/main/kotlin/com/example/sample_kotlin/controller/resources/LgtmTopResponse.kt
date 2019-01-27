7e6e42747d0c13636067485e05437db2282131d7
Yuta <a.yuta.0401@gmnail.com>
2019-01-27 13:56:00 +0900

    create get images function

---
+++ src/main/kotlin/com/example/sample_kotlin/controller/resources/LgtmTopResponse.kt
@@ -0,0 +1,25 @@
+package com.example.sample_kotlin.controller.resources
+
+import java.util.*
+
+/**
+ * フロントに返す用のDTOクラス
+ */
+class LgtmTopResponse {
+    var imageUrl = String()
+    var imageName = String()
+    var imageLgtmUrl = String()
+    var updateDatetime = Date()
+    var isNew = false
+
+    fun buildWithEntity(lgtmEntity: LgtmEntity,baseDate :Date) : LgtmTopResponse{
+        return LgtmTopResponse().apply {
+            imageUrl = lgtmEntity.imageUrl
+            imageName = lgtmEntity.imageName
+            imageLgtmUrl = lgtmEntity.imageLgtmUrl
+            updateDatetime = lgtmEntity.updateDatetime
+            isNew = lgtmEntity.updateDatetime.after(baseDate)
+//            imageLgtmUrl = "[![LGTM]()]()"
+        }
+    }
+}
